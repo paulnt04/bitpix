4292f556b7ea3003c85ec315c57b458a8caf66c8
Paul Panarese <paul@panjunction.com>
2011-10-25 15:07:28 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitpix::Application.config.secret_token = '66938f1c976f3245c400486bf369d85e68fc1a3baebee0455a520a0e06b11ac95cde9f38e095550ec5fb294cfd49ecf3371dcdc4310f689a7cc1673082796f6f'
